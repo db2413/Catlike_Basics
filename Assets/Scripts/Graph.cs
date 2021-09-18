ffa714e64370622f3be73160c0c52ef9c17c776d
Duncan Boyes <boyes.duncan@gmail.com>
2021-09-18 15:54:52 -0700

    Graph of subes with simple default pipeline shader

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] Transform pointPrefab;
+    [SerializeField, Range(10,200)] int resolution = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var position = Vector3.zero;
+        var scale = Vector3.one / resolution;
+        float step = 2f / resolution; // -1 to 1 x range
+
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i+0.5f) * step - 1;
+            position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
