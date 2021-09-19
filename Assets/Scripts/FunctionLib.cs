67b15cadccffa4fd73baf9a67f22f71233203f4c
Duncan Boyes <boyes.duncan@gmail.com>
2021-09-18 17:06:30 -0700

    Multiwave funciton. And ability to choose in editor the funciton

--- Assets/Scripts/FunctionLib.cs
+++ Assets/Scripts/FunctionLib.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 public static class FunctionLib
 {
     public static float Wave (float x, float t) {
-        return Mathf.Sin(Mathf.PI * (x + t));
+        return Sin(PI * (x + t));
+    }
+
+    public static float MultiWave (float x, float t)
+    {
+        float y = Wave(x,t);
+        y += Sin(2*PI * (x + t*0.7f))/2;
+        return y/1.5f;
     }
 
 }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,6 +8,7 @@ public class Graph : MonoBehaviour
     Transform[] points;
     [SerializeField, Range(10,200)] int resolution = 10;
     float time;
+    [SerializeField, Range(0, 1)] int function;
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +38,14 @@ public class Graph : MonoBehaviour
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = FunctionLib.Wave(position.x,time);
+            if (function == 0)
+            {
+                position.y = FunctionLib.Wave(position.x,time);
+            }
+            else
+            {
+                position.y = FunctionLib.MultiWave(position.x,time);
+            }
 
             point.localPosition = position;
         }
